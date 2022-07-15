88e0d0134a0677950b60539db4cd83974164e888
study1994 <zhuzhao1994@163.com>
2022-07-15 08:49:24 +0800

    Update MathJax.js

--- npm/mathjax/latest/MathJax.js
+++ npm/mathjax/latest/MathJax.js
@@ -1,5 +1,5 @@
 (function () {
-    var newMathJax = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js';
+    var newMathJax = 'https://study1994.github.io/study_html/npm/mathjax/MathJax.js';
     var oldMathJax = 'https://study1994.github.io/study_html/npm/mathjax/latest/MathJax.js';
   
     var replaceScript = function (script, src) {
