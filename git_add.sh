5f623e55470efa6bfae4713b8dab0c6d4d76c260
study1994 <zhuzhao1994@163.com>
2022-07-23 08:36:02 +0800

    gitadd

--- git_add.sh
+++ git_add.sh
@@ -1,3 +1,5 @@
+# cp -rf /data3/zzhu/my_use/zhao_gitee/data_code/.history/zhao/* /data3/zzhu/my_use/zhao_gitee/study_html/zhao/*
 git add *
 git commit -m "gitadd"
+# git remote set-url origin https://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@github.com/study1994/study_html.git
 git push origin master
