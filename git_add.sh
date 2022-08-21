1eea0c36a399c2a40cd9f7e1f7144308270a8e8f
zhao-study <zhuzhao1994@163.com>
2022-08-21 10:50:37 +0800

    gitadd

--- git_add.sh
+++ git_add.sh
@@ -4,3 +4,4 @@ git commit -m "gitadd"
 git push
 # git remote set-url origin https://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@github.com/study1994/study_html.git
 # git push origin main
+# OpenSSL SSL_read: Connection was reset, errno 10054 git config --global http.sslVerify "false"
