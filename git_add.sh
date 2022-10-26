ec881d2413060f5b7f424841d07aa4ada5ddd884
朱朝 <zhuzhao@minieye.cc>
2022-10-26 19:24:49 +0800

    gitadd

--- git_add.sh
+++ git_add.sh
@@ -6,3 +6,4 @@ git push
 # git remote set-url origin https://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@github.com/study1994/study_html.git
 # git push origin main
 # OpenSSL SSL_read: Connection was reset, errno 10054 git config --global http.sslVerify "false"
+# 都是小写
